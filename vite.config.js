369a2a4ca3d33a277a559bab48c6a9f63964fadc
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-08 08:50:51 +0300

    Update build step

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'batchRunner',
+      formats: ['umd', 'cjs', 'es'],
+      fileName: (format) => {
+        if (format === 'umd') return 'batch-runner.umd.js'
+        if (format === 'cjs') return 'batch-runner.cjs'
+        return 'batch-runner.esm.mjs'
+      }
+    },
+    minify: 'esbuild'
+  }
 })
